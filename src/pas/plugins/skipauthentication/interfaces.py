a67eb8a4b48413af336c41c21487fea95aa1ca1d
Martin Peeters <martin.peeters@affinitic.be>
2016-12-28 09:21:12 +0100

    Add a configlet to manage the master password

--- src/pas/plugins/skipauthentication/interfaces.py
+++ src/pas/plugins/skipauthentication/interfaces.py
@@ -7,8 +7,11 @@ Created by mpeeters
 :license: GPL, see LICENCE.txt for more details.
 """
 
+from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+from pas.plugins.skipauthentication import _
 
 
 class IPasPluginsSkipauthenticationLayer(IDefaultBrowserLayer):
@@ -17,3 +20,15 @@ class IPasPluginsSkipauthenticationLayer(IDefaultBrowserLayer):
 
 class ISkipAuthenticationPlugin(Interface):
     """Skip Authentication Plugin"""
+
+
+class ISkipAuthenticationSettings(Interface):
+    """Skip Authentication Plugin settings"""
+
+    password = schema.TextLine(
+        title=_(u'Master password'),
+        description=_(
+            u'Password used for the authentication'
+        ),
+        required=True,
+    )
